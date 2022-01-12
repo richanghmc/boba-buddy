2d4d640ae986d1b65127375c0f874e189751aa63
richanghmc <sg626.richardchang@gmail.com>
2022-01-11 16:56:59 -0800

    inbox and chat

--- bobaBuddyApp/app.py
+++ bobaBuddyApp/app.py
@@ -13,3 +13,11 @@ def login():
 @app.route("/feed/")
 def feed():
     return render_template('feed.html')
+
+@app.route("/messages/")
+def messages():
+    return render_template('messages.html')
+
+@app.route("/inbox/")
+def inbox():
+    return render_template('inbox.html')
