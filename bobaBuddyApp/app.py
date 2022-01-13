ad96e9ca4289e1258d12b90b079abd47dd650cfa
richanghmc <sg626.richardchang@gmail.com>
2022-01-12 16:14:43 -0800

    data in one file. can navigate through app

--- bobaBuddyApp/app.py
+++ bobaBuddyApp/app.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
+from data import users, userIdValues, default
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def login():
     return render_template('login.html')
 
 @app.route("/profile/")
-def login():
+def profile():
     return render_template('profile.html')
 
 @app.route("/feed/")
 def feed():
     return render_template('feed.html')
 
-@app.route("/messages/")
-def messages():
-    return render_template('messages.html')
+@app.route("/messages/<int:value>")
+def messages(value):
+    user = userIdValues[value]
+    return render_template('messages.html',user_data=user, default_response= default)
 
 @app.route("/inbox/")
 def inbox():
-    return render_template('inbox.html')
+    return render_template('inbox.html', users=users)
---
+++ bobaBuddyApp/data.py
@@ -0,0 +1,49 @@
+default = {
+    "message1" : "Sure!",
+    "message2" : "Yea, sounds good!"
+
+}
+personA = {
+    "idValue" : 1,
+    "username" : "Person A",
+    "message1" : "Hey let's get boba!",
+    "message2" : "Awesome! Does Saturday work?",
+    "message3" : "Cool, see you then!"
+}
+
+personB = {
+    "idValue" : 2,
+    "username" : "Person B",
+    "message1" : "Want to grab a drink?",
+    "message2" : "Wednesday?",
+    "message3" : "Cool, cya."
+}
+
+personC = {
+    "idValue" : 3,
+    "username" : "Person C",
+    "message1" : "You down to get some boba?",
+    "message2" : "Nice! How does Friday sound?",
+    "message3": "Alrighty, looking forward to it"
+}
+personD = {
+    "idValue" : 4,
+    "username" : "Person D",
+    "message1" : "Me, you, boba?",
+    "message2" : "Cool cool. Sunday?",
+    "message3" : "Sweet, see you later"
+}
+
+users = [
+    personA,
+    personB,
+    personC,
+    personD
+]
+
+userIdValues = {
+    1 : personA,
+    2 : personB,
+    3 : personC,
+    4 : personD
+}
